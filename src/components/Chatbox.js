f94755feff9bf1c8fc63a3128f07fc6db42b50cd
Azem <azementhusy@gmail.com>
2024-10-04 14:09:35 +0100

    before ChatBubble

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
 import ChatInput from './ChatInput'
 import ChatResponse from './ChatResponse';
+import {Box, Grid} from '@mui/material';
 
 
 const Chatbot = () => {
     const [inputText, setInputText] = useState("");
     const [articles, setArticles] = useState([]);
     const [chatResponse, setChatResponse] = useState("");
+    const [loading, setLoading]=useState(false);
     
     function handleChange(event){
         const newValue = event.target.value;
@@ -15,6 +17,13 @@ const Chatbot = () => {
 
     const handleSubmit = async (event) =>{
         event.preventDefault();
+
+        if (inputText.trim().length === 0) {
+            setChatResponse("Please at least write something~");
+            return;
+        }
+
+        setLoading(true);
 
         try {
             const gptResponse = await fetch('http://localhost:5000/api/generate',{
@@ -45,15 +54,21 @@ const Chatbot = () => {
                 console.error('Request to OpenAlex API failed', apiResponse.status);
                 setChatResponse("Sorry, I couldn't retrieve the data from OpenAlex.")
             }
+            setLoading(false)
         } catch (error) {
+            setLoading(false);
+            if (error.response && error.response.status === 404) {
+                setChatResponse("Sorry, no results found for your query.");
+            } else {
+                setChatResponse("An unexpected error occurred. Please try again later.");
+            }
             console.error('Error calling APIs:', error);
-            setChatResponse("An error occurred. Please try again.");
         }
         setInputText("")
     };
 
     return (
-      <div className="chatbox">
+      <Box className="chatbox" p={3}>
         <h1>Research Article Chatbot</h1>
         <ChatInput 
             inputText = {inputText}
@@ -61,13 +76,16 @@ const Chatbot = () => {
             handleSubmit = {handleSubmit}
         />
 
-        <div>
+        <Box mt={2}>
             <h2>Chat Response:</h2>
-            <p>{chatResponse || "No response yet"}</p>
-        </div>
-        <ChatResponse articles = {articles}/>
-      </div>
+            {loading? <p>Loading...</p> : <p>{chatResponse || "No response yet"}</p>}
+        </Box>
+
+        <Grid container spacing={2} mt={3}>
+            <ChatResponse articles = {articles}/>
+        </Grid>
+      </Box>
     );
-  };
+};
   
-  export default Chatbot;
+export default Chatbot;
--- src/components/CreateLists.jsx
+++ src/components/CreateLists.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import {Card, CardContent, Typography} from '@mui/material';
 
 function CreateLists({
